c73baf7b6cdb8854728d2a524c2d01d8112c05ea
Eliezer Graber <me@eygraber.com>
2023-11-17 16:03:10 -0500

    WIP: k2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,6 +31,9 @@ pluginManagement {
     }
 
     gradlePluginPortal()
+
+    maven(url = "https://androidx.dev/storage/compose-compiler/repository/")
+    maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
   }
 }
 
