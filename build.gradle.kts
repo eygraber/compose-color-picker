e1f19fe3a99211bb19d12aedd661d83ab83d354b
Eliezer Graber <me@eygraber.com>
2026-01-28 00:38:46 -0500

    Update Detekt v2 to alpha 2 (#581)

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,11 +31,7 @@ gradleConventionsDefaults {
   }
 
   detekt {
-    plugins(
-      libs.detektCompose,
-      libs.detektEygraber.formatting,
-      libs.detektEygraber.style,
-    )
+    plugins(libs.detektCompose)
   }
 
   kotlin {
